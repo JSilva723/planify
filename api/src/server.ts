6ec5a8cb1bde762dc6a06eb3f95dbab2859deed1
JSilva723 <silvajonatanivan@gmail.com>
2024-04-03 16:51:15 -0300

    feat(api): add services and slots routes

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,16 @@
 import express from 'express'
+import { slots, services } from './utils'
 
 export const server = () => {
     const app = express()
     app.get('/ping', (req, res) => {
         res.send({ msg: 'pong' })
+    })
+    app.get('/services', (req, res) => {
+        res.send({ services })
+    })
+    app.get('/slots', (req, res) => {
+        res.send(slots)
     })
 
     return app
