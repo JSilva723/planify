a809f0e35436558f860acf93e357206a415299fc
JSilva723 <silvajonatanivan@gmail.com>
2024-04-05 20:19:22 -0300

    feat(app): add styles in not found

--- app/src/pages/not-found/NotFound.tsx
+++ app/src/pages/not-found/NotFound.tsx
@@ -1,5 +1,19 @@
 import React from 'react'
+import styles from './NotFound.module.css'
+import { Button } from '@components/button/Button'
+import { useNavigate } from 'react-router-dom'
 
 export const NotFound = () => {
-    return <div>Ups!</div>
+    const navigate = useNavigate()
+
+    return (
+        <div className={styles.container}>
+            <h1>Ups</h1>
+            <Button 
+                label='Go to Reserve'
+                active
+                onClick={() => navigate('reserve')}
+            />
+        </div>
+    )
 }
