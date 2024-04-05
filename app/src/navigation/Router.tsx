8ae78a18df0b84e0dde6e1baf06dc275235026d2
JSilva723 <silvajonatanivan@gmail.com>
2024-04-05 20:00:14 -0300

    feat(app): change router

--- app/src/navigation/Router.tsx
+++ app/src/navigation/Router.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { createBrowserRouter } from 'react-router-dom'
+import { createHashRouter } from 'react-router-dom'
 import { NotFound } from '@pages/not-found/NotFound'
 import { MyTurns } from '@pages/my-turns/MyTurns'
 import { Welcome } from '@pages/welcome/Welcome'
 import { Reserve } from '@pages/reserve/Reserve'
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
     {
         path: '/',
         element: <Welcome />,
